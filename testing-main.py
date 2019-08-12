6ebcb32a47fb39f63769c1148475ad132e1c677c
kennedyengineering <completely@fake.email>
2019-08-12 11:54:17 -0700

    Group sort works

--- testing-main.py
+++ testing-main.py
@@ -90,6 +90,7 @@ class WebcamVideoStream:
 # end helper module definition
 
 videoSource = "rtsp://LPRuser:ThisISfun1@10.48.140.5:554/Streaming/channels/101/" # bring from config file. Allow for multiple cameras
+#videoSource = "recording.avi"
 cam = WebcamVideoStream(src=videoSource).start()
 fps = FPS().start()
 
@@ -219,6 +220,7 @@ class licenseplateService():
 		self.notified = False
 
 		self.orderedLicensePlateList = []
+		self.groupsList = []
 
 		#self.outputfile = open("timedensity.txt", "w+")
 
@@ -241,53 +243,109 @@ class licenseplateService():
 				tempLicensePlateList = self.detectionBoxReference.licenseplateList
 				self.detectionBoxReference.licenseplateList = []
 
-				#licensePlateListLength = len(self.detectionBoxReference.licenseplateList)
-				#print("Added plate to list. Current length: ", licensePlateListLength)
-
 				# use licensePlateList like a buffer of new data to be actively sorted into a new ordered list
 				# order based on time stamp
 				tempLicensePlateList.sort(key=lambda x: x.timeSpotted)
 
-				# diagnostic for loop
-				for plate in tempLicensePlateList:
-					print(plate.timeSpotted)
-					#print(plate.timeSpotted)
-
-				# add ordered list to total list
+				# add ordered list to total list, merge
 				self.orderedLicensePlateList += tempLicensePlateList
 
-				# ordered list based on time, separate into groups based on time differentials
-				# refine groups based on license plate numbers
-				timeDifferentialIndexes = []				# time differential index list, difference between [i+1] - [i]
-				minTimeDifferential = 3 # seconds
-				for i in range(len(self.orderedLicensePlateList)):
-					# check if last element in list
-					if i == len(self.orderedLicensePlateList)-1:
-						break
+			########### PROCESS PLATES IN THIS THREAD LOOP ############
 
-					difference = self.orderedLicensePlateList[i+1].timeSpotted - self.orderedLicensePlateList[i].timeSpotted
-					print(difference)
+			########### BEGIN SORTING PLATES INTO GROUPS ##############
 
-					if difference >= minTimeDifferential:
-						timeDifferentialIndexes.append(i)
+			# ordered list based on time, separate into groups based on time differentials
+			# refine groups based on license plate numbers
+			timeDifferentialIndexes = []				# time differential index list, difference between [i+1] - [i]
+			minTimeDifferential = 3 					# seconds, should be variable, the time differential between sequential plates time spotted
+			maxWaitTime = 10							# seconds, should be variable, the time differential between time spotted and system time
+			for i in range(len(self.orderedLicensePlateList)):
+				# check if last element in list
+				if i == len(self.orderedLicensePlateList)-1:
+					break
 
-				groups = []
-				for i in range(len(timeDifferentialIndexes)):
-					# check if last element in list
-					if i == len(timeDifferentialIndexes)-1:
-						break
+				difference = self.orderedLicensePlateList[i+1].timeSpotted - self.orderedLicensePlateList[i].timeSpotted
 
-					indexRange = range(timeDifferentialIndexes[i], timeDifferentialIndexes[i+1]) # goes from numbers i --> one less than i+1
-					groups.append(indexRange)
+				# system difference results in all plates being timeDifferentials if another car does not pass with in the waittime interval
+				# needs to work the orderedLicensePlateList in reverse, so the LATEST plate is counted down
+				# conclusion: bring system difference outside of the for loop and operate on the last element in the orderedLicenseplate List!
+				#systemDifference = getSystemUptime() - self.orderedLicensePlateList[i].timeSpotted # errors out if plates are not being removed...
 
-				print(timeDifferentialIndexes)
-				print(groups)
+				#print(difference, systemDifference)
 
-				#self.outputfile.write(str(datetime.datetime.now().time())+"\n")
-				#print(uptimeToSeconds(self.detectionBoxReference.licenseplateList[licensePlateListLength-1].timeSpotted))
+				#if (difference >= minTimeDifferential) or (systemDifference >= maxWaitTime): # include systemDifference
+				if difference >= minTimeDifferential: # do not include systemDifference
+					timeDifferentialIndexes.append(i)
+
+				# works great!
+				# one flaw:
+				# 	needs a second plate to create a time differential thus allowing the previous plate to be processed
+				#	should place and active timer in place of a differential for stability
+				#	a timing thread for each group?
+				#	reset a timer every time a new plate is detected, add plate to group. If timer runs out, group is sealed?
+				# 	system difference has not been tested, possible flaws may exist, solves the last car problem
+
+			# find system difference with the LAST plate in the list. operate if self.orderedLicensePlateList is NOT empty
+			if len(self.orderedLicensePlateList) != 0:
+				systemDifference = getSystemUptime() - self.orderedLicensePlateList[len(self.orderedLicensePlateList)-1].timeSpotted
+				if systemDifference >= maxWaitTime:
+					timeDifferentialIndexes.append((len(self.orderedLicensePlateList)-1))
+
+			#print(timeDifferentialIndexes)
+
+			groups = []										# groupings of indexes, grouped by time differentials calculated above
+			lastIndex = 0
+			for index in timeDifferentialIndexes:
+				group = range(lastIndex, index + 1)
+				lastIndex = index + 1
+				groups.append(group)
+			# needs testing but seems to work
+
+			#print(groups)
+
+			# groups is a list of ranges, use them to remove plates from orderedList and put them in a list inside of self.groups list
+			indexDecrement = 0		# everytime a plate is deleted, indexDecrement goes up one. This combats the index shift that occurs in a list of changing size
+			for group in groups: # AKA for range in ranges...
+				tempPlateGroup = []
+				for index in group:
+					plate = self.orderedLicensePlateList[index -indexDecrement]
+					indexDecrement += 1;
+					tempPlateGroup.append(plate)
+					self.orderedLicensePlateList.remove(plate) # REMOVES PLATE FROM LIST!!! Watchout for remove-erase problems
+				self.groupsList.append(tempPlateGroup)
+
+
+			print(len(self.groupsList))
+			#print(self.groupsList)
+
+
+			################# PLATE GROUP SORTING END ################
+
+			################# REFINE GROUPS ###################
+
+			# take the plates in self.groupsList and check for consistency
+			# refine the groups by moving plates into their correct groups if they were miss-classified
+			# verify by plate number?
+
+			################## END REFINE GROUPS ##############
+
+			################## PUBLISH DATA TO SQL DATABASE ####################
+
+			# might be a good idea to do this in a different thread if it takes to long. Avoid missing notifications at all cost!
+			# actually, buffer flushing happens in this thread, so its not too big of deal, but still avoid it if you can
+
+			# need to record everything in the licenseplate class
+			# - plate number
+			# - path to plate image (one image will be saved)
+			# - time spotted (range?)
+			# - camera name
+			# - detectorBoxName (IN/OUT)
+
+
+
+			################## END PUBLISH DATA TO SQL DATABASE ################
 
 	def stop(self):
-		#self.outputfile.close()
 		self.stopped = True
 
 	def notify(self):
