afafe97efbdb117ccc5bb0acbd58e8b744fb6115
kennedyengineering <completely@fake.email>
2019-09-25 17:23:45 -0700

    Modified webcam class for network outage redundancy

--- testing-main.py
+++ testing-main.py
@@ -62,8 +62,10 @@ class WebcamVideoStream:
 	def __init__(self, src=0):
 		# initialize the video camera stream and read the first frame
 		# from the stream
+		self.src = src
 		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
+		#(self.grabbed, self.frame) = self.stream.read()
+		self.frame = self.grabFrame()
 
 		# initialize the variable used to indicate if the thread should
 		# be stopped
@@ -74,6 +76,19 @@ class WebcamVideoStream:
 		Thread(target=self.update, args=()).start()
 		return self
 
+	def grabFrame(self):
+		# handle when self.stream.read() returns None as a frame
+		# network outage redundancy
+		(grabbed, frame) = self.stream.read()
+		while not grabbed:
+			if self.stopped:	# allow clean exit with the rest of the program
+				return
+
+			self.stream = cv2.VideoCapture(self.src) # initialize a new video stream to compensate for broken network connection, slow an inefficient, can be improved but it works
+			(grabbed, frame) = self.stream.read()
+
+		return frame
+
 	def update(self):
 		# keep looping infinitely until the thread is stopped
 		while True:
@@ -82,7 +97,8 @@ class WebcamVideoStream:
 				return
 
 			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
+			self.frame = self.grabFrame()
+			#grabbed, self.frame = self.stream.read()
 
 	def read(self):
 		# return the frame most recently read
